934c7dbebefb5e3f9913cb60877ef487c0f84bea
tianyuemylove <2112471568@qq.com>
2018-05-28 15:21:54 +0800

    循环.py

--- 循环1～100.py
+++ 循环1～100.py
@@ -5,9 +5,9 @@ while True:
     p = int(input('请输入1～100数字'))
     i += 1
     if p > m:
-        print('猜大了')
+        print('猜大了, ')
     elif p < m:
-        print('猜小了')
+        print('猜小了 ')
     else:
-        print('耶，猜对了')
+        print('耶，猜对了 ，买冰淇淋')
         break
