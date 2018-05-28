2851d8001d6e35035a2fdc9b41a30590876c543a
tianyuemylove <2112471568@qq.com>
2018-05-28 15:15:19 +0800

    循环1～100.py

---
+++ 循环1～100.py
@@ -0,0 +1,13 @@
+import random
+m = random.randint(1,100)
+i = 0
+while True:
+    p = int(input('请输入1～100数字'))
+    i += 1
+    if p > m:
+        print('猜大了')
+    elif p < m:
+        print('猜小了')
+    else:
+        print('耶，猜对了')
+        break
