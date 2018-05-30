8bbacdd1b7d01531371bce6e032322bd50808cd1
tianyuemylove <2112471568@qq.com>
2018-05-30 19:47:43 +0800

    cc.py

--- cc.py
+++ cc.py
@@ -1,5 +1,11 @@
-def y ():
-    print ("#" * 500)
-    print ("@" * 30)
-    print ("*"* 20)
+chengji = int(input('请输入成绩'))
+if chengji > 80:
+    print('优秀')
+elif chengji >60 and chengji <= 80:
+    print('及格')
+    break
+else:
+    print('不及格')
+
+else:
 
